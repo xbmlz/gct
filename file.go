551b7e606d3c4f9fb605f2d7859bb37b54cafabb
chenxc <997909544@qq.com>
2021-07-29 13:10:12 +0800

    feat: add AppendString

--- file.go
+++ file.go
@@ -1,6 +1,22 @@
 package gct
 
-import "os"
+import (
+	"bufio"
+	"os"
+)
+
+// AppendString 将String写入文件，追加模式
+func (f *File) AppendString(content string, path string) (*os.File, error) {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	write := bufio.NewWriter(file)
+	write.WriteString(content)
+	write.Flush()
+	return file, nil
+}
 
 // Exist 判断文件是否存在
 func (f *File) Exist(path string) bool {
--- file_test.go
+++ file_test.go
@@ -2,6 +2,15 @@ package gct
 
 import "testing"
 
+func TestAppendString(t *testing.T) {
+	path := "./testdata/file.txt"
+	_, err := FileUtils.AppendString("test append string", path)
+	if err != nil {
+		t.Error("file append error")
+		return
+	}
+}
+
 func TestExist(t *testing.T) {
 	path := "./file.go"
 	if !FileUtils.Exist(path) {
